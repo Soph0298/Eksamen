9fdfa0cfd945186521a4801c9091672732616b79
Your Name <Soph0298@stud.kea.dk>
2021-06-07 21:57:46 +0200

    add cv tekst

--- javascript.js
+++ javascript.js
@@ -2,8 +2,22 @@ window.addEventListener("load", SidenVises);
 
 function SidenVises() {
   console.log("Siden vises");
+  document.querySelector("#button").classList.add("puls");
+  document.querySelector("#button").addEventListener("click", klik);
+}
+
+function klik() {
+  console.log("knap klikket");
+  document.querySelector("#button").classList.add("hide");
+
   document.querySelector("#element1").classList.add("right");
   document.querySelector("#element2").classList.add("down");
   document.querySelector("#element3").classList.add("up");
   document.querySelector("#element4").classList.add("left");
+  document.addEventListener("animationend", Hideelements);
+}
+
+function Hideelements() {
+  console.log("Gem elementerne");
+  document.querySelector("#puzzle").classList.add("hide");
 }
