1425455baa00cd729eefac90919b6b5716059e81
Your Name <Soph0298@stud.kea.dk>
2021-06-08 23:09:19 +0200

    forside mobil færdig

--- javascript.js
+++ javascript.js
@@ -2,22 +2,28 @@ window.addEventListener("load", SidenVises);
 
 function SidenVises() {
   console.log("Siden vises");
-  document.querySelector("#button").classList.add("puls");
+  document.querySelector("nav").classList.add("hide");
   document.querySelector("#button").addEventListener("click", klik);
 }
 
 function klik() {
   console.log("knap klikket");
   document.querySelector("#button").classList.add("hide");
-
   document.querySelector("#element1").classList.add("right");
   document.querySelector("#element2").classList.add("down");
   document.querySelector("#element3").classList.add("up");
   document.querySelector("#element4").classList.add("left");
-  document.addEventListener("animationend", Hideelements);
+  document.querySelector("#element5").classList.add("down");
+
+  document.addEventListener("animationend", hideElements);
 }
 
-function Hideelements() {
+function hideElements() {
   console.log("Gem elementerne");
-  document.querySelector("#puzzle").classList.add("hide");
+  document.querySelector("#element1").classList.add("hide");
+  document.querySelector("#element2").classList.add("hide");
+  document.querySelector("#element3").classList.add("hide");
+  document.querySelector("#element4").classList.add("hide");
+  document.querySelector("nav").classList.remove("hide");
+  document.querySelector("nav").classList.add("opacity");
 }
