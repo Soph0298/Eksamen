cb86d94bb9226e0ca2ad4519cd4a6997bd510597
Your Name <Soph0298@stud.kea.dk>
2021-06-09 12:39:12 +0200

    Mobile oversigtside tema2, 3 og 4

--- javascript.js
+++ javascript.js
@@ -1,38 +1,41 @@
-window.addEventListener("load", SidenVises);
+// window.addEventListener("load", SidenVises);
 
-function SidenVises() {
-  console.log("Siden vises");
-  document.querySelector("nav").classList.add("hide");
-  document.querySelector("#tema02").classList.add("hide");
-  document.querySelector("#tema03").classList.add("hide");
-  document.querySelector("#tema04").classList.add("hide");
-  document.querySelector("#tema05").classList.add("hide");
-  document.querySelector("#temaCV").classList.add("hide");
-  document.querySelector("#button").addEventListener("click", klik);
-}
+// function SidenVises() {
+//   console.log("Siden vises");
+//   document.querySelector("nav").classList.add("hide");
+//   document.querySelector("#temaer_hide").classList.add("hide");
+//   document.querySelector("#button").addEventListener("click", klik);
+// }
 
-function klik() {
-  console.log("knap klikket");
-  document.querySelector("#button").classList.add("hide");
-  document.querySelector("#element1").classList.add("right");
-  document.querySelector("#element2").classList.add("down");
-  document.querySelector("#element3").classList.add("up");
-  document.querySelector("#element4").classList.add("left");
-  document.querySelector("#element5").classList.add("down");
+// function klik() {
+//   console.log("knap klikket");
+//   document.querySelector("#button").classList.add("hide");
+//   document.querySelector("#element1").classList.add("right");
+//   document.querySelector("#element2").classList.add("down");
+//   document.querySelector("#element3").classList.add("up");
+//   document.querySelector("#element4").classList.add("left");
+//   document.querySelector("#element5").classList.add("down");
 
-  document.addEventListener("animationend", hideElements);
-}
+//   document.addEventListener("animationend", hideElements);
+// }
 
-function hideElements() {
-  console.log("Gem elementerne");
-  document.querySelector("#element1").classList.add("hide");
-  document.querySelector("#element2").classList.add("hide");
-  document.querySelector("#element3").classList.add("hide");
-  document.querySelector("#element4").classList.add("hide");
-  document.querySelector("#tema02").classList.remove("hide");
-  document.querySelector("#tema03").classList.remove("hide");
-  document.querySelector("#tema04").classList.remove("hide");
-  document.querySelector("#tema05").classList.remove("hide");
-  document.querySelector("#temaCV").classList.remove("hide");
-  document.querySelector("nav").classList.remove("hide");
-}
+// function hideElements() {
+//   console.log("Gem elementerne");
+//   document.querySelector("#element1").classList.add("hide");
+//   document.querySelector("#element2").classList.add("hide");
+//   document.querySelector("#element3").classList.add("hide");
+//   document.querySelector("#element4").classList.add("hide");
+//   document.querySelector("#temaer_hide").classList.remove("hide");
+//   document.querySelector("nav").classList.remove("hide");
+// }
+
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector("nav");
+
+// hamburger.addEventListener("click", mobilMenu);
+
+// function mobilMenu() {
+//   console.log("burgermenu");
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// }
