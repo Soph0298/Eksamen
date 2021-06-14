cdabb0f4344ea5140d4bb08c0599e8c00ab8ffd6
Soph0298 <Soph0298@stud.kea.dk>
2021-06-14 22:50:20 +0200

    orange hero set

--- java02.js
+++ java02.js
@@ -1,6 +1,6 @@
-window.addEventListener("load", SidenVises);
+// window.addEventListener("load", SidenVises);
 
-function SidenVises() {
-  console.log("Siden vises");
-  document.querySelector("#hero_section_02").classList.add("dropdown");
-}
+// function SidenVises() {
+//   console.log("Siden vises");
+//   document.querySelector("#hero_section_02").classList.add("dropdown");
+// }
