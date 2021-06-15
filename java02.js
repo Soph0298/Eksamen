f97508b48618b5ed9762a3d875e43166ae765d41
Soph0298 <Soph0298@stud.kea.dk>
2021-06-15 15:04:17 +0200

    white box with shadow - layout done

--- java02.js
+++
@@ -1,6 +0,0 @@
-// window.addEventListener("load", SidenVises);
-
-// function SidenVises() {
-//   console.log("Siden vises");
-//   document.querySelector("#hero_section_02").classList.add("dropdown");
-// }
--- java03.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", SidenVises);
-
-function SidenVises() {
-  console.log("Siden vises");
-  document.querySelector("#hero_section_03").classList.add("bringleft");
-}
